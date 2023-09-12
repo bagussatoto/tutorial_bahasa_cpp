4b82232237c8b648b03cabf74bac2d13cc171791
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2023-09-12 21:03:16 +0700

    main : `Hello World``

--- tutorial-1/1.Hello World.cpp
+++ tutorial-1/1.Hello World.cpp
@@ -6,5 +6,9 @@ int main()
     //dalam cpp untuk meng output kan sebuah kalimat, diperlukan cout<< dengan tulisan dalam tanda kutip,
      //karna ia dibaca sebagai string
     cout<<"Hello World !";
+
+    //digunakan dalam semua fungsi, baik fungsi yang memiliki tipe data kembalian (return type) 
+    //maupun fungsi yang tidak memiliki tipe data kembalian (void). 
+    //Namun, jika fungsi tidak memiliki tipe data kembalian, maka nilai return 0; tidak wajib dituliskan.
     return 0;
 }
