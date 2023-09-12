6d2ae6d38521229067e7ce8da1c4905b9685fd9a
BAGUS BUDI SATOTO <79775593+bagussatoto1@users.noreply.github.com>
2023-09-12 20:55:48 +0700

    main : ``variable``

---
+++ tutorial/3.Variabel.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ //int main wajib dalam sebuah codingan cpp, dia sebagai "body" utama c++
+int main()
+{
+   //disini saya deklarasi sebuah variabel bernama x
+   string x;
+   //disini saya output sebuah tulisan =
+   cout<<"Isi Namamu = ";
+   //variabel x diisi dengan inputan dari user
+   cin>>x;
+   //kemudian kita cout variabel yang telah kita isi 
+   cout<<"hai, "<<x;
+}
