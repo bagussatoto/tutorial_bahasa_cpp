e73c9085a8751adf98260f2a29cc1ab37f90973b
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2023-09-12 20:55:01 +0700

    main : ``deklarasi variable``

---
+++ tutorial/2.Assign Variabel.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ //int main wajib dalam sebuah codingan cpp, dia sebagai "body" utama c++
+int main()
+{
+  //disini saya deklarasikan sebuah variabel dengan nama X
+  string x;
+  int angka;
+  //disini saya isi variabel x dengan sebuah string
+  x = "Halo variabel";
+  angka = 5;
+  //disini saya output kan nilai dari variabel x
+  cout<<x; 
+  cout<<angka;
+}
